71fdc7a932e0e09efa1540c39b6d8ca90ef67df8
Christopher.pantoja <christopher.pantoja@nextbasket.com>
2024-12-17 16:44:15 +0800

    Add files, ignore temp files, update products with new properties'

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -6,11 +6,14 @@ class Product
 {
     private $row;
     private $headers;
+    private $total;
+    private $key;
 
     public function __construct(array $data, array $headers)
     {
         $this->row     = array_combine($headers, $data);
         $this->headers = $headers;
+        $this->total   = 1;
     }
 
     public function get($property)
@@ -27,7 +30,8 @@ class Product
 
         $concatenated = implode('|', $normalized);
         
-        return hash('sha256', $concatenated);
+        $this->key = hash('sha256', $concatenated);
+        return $this->key;
     }
 
     public function getHeaders()
@@ -38,5 +42,21 @@ class Product
     public function getOriginalRow()
     {
         return $this->row;
+    }
+
+    public function incrementCount()
+    {
+        $this->total++;
+        return $this;
+    }
+    
+    public function getTotal()
+    {
+        return $this->total;
+    }
+    
+    public function getKey()
+    {
+        return $this->key;
     }
 }
